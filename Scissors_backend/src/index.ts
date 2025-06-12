ad623ee0b1b20e66fa4ff0e45ebdb86d97ef8af5
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2025-06-12 21:36:30 +0530

    updated index.js file

--- Scissors_backend/src/index.ts
+++ Scissors_backend/src/index.ts
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = ["https://scissors-a-salon-service.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173,https://scissors-a-salon-service.vercel.app/"];
 app.use(
   cors({
     origin: allowedOrigins,
